349b4e567acf435fa9207d337142ab350af2cee7
Rizwan Ahsan <38108448+Lazarus-XD@users.noreply.github.com>
2020-06-02 22:18:04 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,15 +4,21 @@ warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 ##################      CODE STARTS HERE      #####################
 import pygame
+import time
 from boards import *
 pygame.init()
 
+#Window, title and icons
 win = pygame.display.set_mode((730, 900))
 pygame.display.set_caption("SUDOKU")
 icon = pygame.image.load("icon.png")
 pygame.display.set_icon(icon)
+play_button = pygame.image.load("play_button.png")
+play_button = pygame.transform.smoothscale(play_button, (40, 40))
+pause_button = pygame.image.load("pause_button.png")
+pause_button = pygame.transform.smoothscale(pause_button, (40, 40))
 
-#sound
+#Sound
 pygame.mixer.music.load("bg_music.mp3")
 pygame.mixer.music.play(-1)
 
@@ -23,6 +29,7 @@ GREEN = (0, 212, 14)
 BLUE = (191, 218, 255)
 GREY = (191, 191, 191)
 BLACK = (0, 0, 0)
+BUTTON = (16, 187, 235)
 
 
 class Board():
@@ -41,6 +48,7 @@ class Board():
         self.selected = False
         self.xPos = None
         self.yPos = None
+        self.click = False
         #Creates another identical non-aliased initial board
         for i in range(9):
             for j in range(9):
@@ -71,7 +79,7 @@ class Board():
     def mouseOnGrid(self):
         """
         Gets the mouse position when pressed inside the grid
-        :return: tuple or boolean
+        :return: tuple or bool
         """
         if self.mousePos[0] > self.x and self.mousePos[0] < self.x + self.gridSize:
             if self.mousePos[1] > self.y and self.mousePos[1] < self.y + self.gridSize:
@@ -91,7 +99,7 @@ class Board():
     def highlightSelected(self):
         """
         Takes input of the mouse to return the width, height of mouse inside grid
-        :return: blue rect around the grid selected
+        :return: rect around the grid selected
         """
         if self.mouseOnGrid():
             self.xPos = (self.mousePosition()[1] * self.cellSize) + self.x
@@ -99,6 +107,12 @@ class Board():
             pygame.draw.rect(self.win, BLUE, (self.xPos, self.yPos, self.cellSize, self.cellSize))
 
     def displayNumbers(self, win, board):
+        """
+        takes the passed in board list and draws number on the window
+        :param win: win
+        :param board: list of lists
+        :return: numbers on board
+        """
         y_change = 0
         x_change = 0
 
@@ -106,6 +120,7 @@ class Board():
             for j in range(9):
                 if board[i][j] != 0:
                     value = self.text.render(str(board[i][j]), 1, BLACK)
+                    #draws a grey rect around the grid containing numbers when the initial board is passed
                     if board == self.initialBoard:
                         pygame.draw.rect(win, GREY, (self.x + x_change, self.y + y_change, self.cellSize, self.cellSize))
                     win.blit(value, (self.x + x_change + 19, self.y + y_change + 10))
@@ -119,7 +134,7 @@ class Board():
         """
         Inserts number 1-9 in the edited board
         :param win: win
-        :return: number in the grid
+        :return: int
         """
         if self.mouseOnGrid():
             if self.initialBoard[self.mousePosition()[0]][self.mousePosition()[1]] == 0:
@@ -138,6 +153,11 @@ class Board():
         self.editedBoard[number[0]][number[1]] = 0
 
     def isEmpty(self, board):
+        """
+        checks for empty space in board as 0 and returns the index as tuple else returns false
+        :param board: list of lists
+        :return: tuple or bool
+        """
         for row in range(9):
             for col in range(9):
                 if board[row][col] == 0:
@@ -146,6 +166,14 @@ class Board():
         return False
 
     def checkCorrect(self,board, num, row, col):
+        """
+        checks the inputed number with row, column and smaller grid
+        :param board: list of lists
+        :param num: int
+        :param row: int
+        :param col: int
+        :return: bool
+        """
         # check row
         for i in range(9):
             if board[row][i] == num and i != col:
@@ -167,6 +195,10 @@ class Board():
         return True
 
     def solver(self):
+        """
+        implement the backtracking algorithm to solve the board
+        :return: solved sudoku board
+        """
         value = self.isEmpty(self.editedBoard)
 
         if value:
@@ -199,6 +231,9 @@ class Board():
         return False
 
     def editCheckerBoard(self):
+        """
+        solves the boardChecker list and used to compare with the player input
+        """
         value = self.isEmpty(self.boardChecker)
 
         if value:
@@ -217,39 +252,85 @@ class Board():
 
         return False
 
-    def checkIfCorrect(self):
+    def checkInputIfCorrect(self):
+        """
+        checks the player input with the solved boardChecker list
+        """
         row, col = self.mousePosition()
         if self.editedBoard[row][col] != self.boardChecker[row][col]:
             if self.editedBoard[row][col] != 0:
                 pygame.draw.rect(win, RED, (self.xPos, self.yPos, self.cellSize, self.cellSize), 4)
+
+    def inputBoxes(self, win):
+        text = pygame.font.SysFont("calibri", 30, 1)
+        newGame = text.render("NEW GAME", 1, BLACK)
+
+        pygame.draw.rect(win, BUTTON, (self.x + 25, 160, 160, 40))
+        pygame.draw.rect(win, BLACK, (self.x + 25, 160, 160, 40), 3)
+        pygame.draw.rect(win, BUTTON, (self.x + self.gridSize - 105, 160, 105, 40))
+        pygame.draw.rect(win, BLACK, (self.x + self.gridSize - 105, 160, 105, 40), 3)
+        win.blit(newGame, (self.x + 32, 166))
+
+        if self.click:
+            win.blit(play_button, (self.x + self.gridSize - 159, 160))
+        else:
+            win.blit(pause_button, (self.x + self.gridSize - 159, 160))
+
 
 def reDrawGameWindow(win):
     win.fill(WHITE)
     if board.mousePos:
         board.highlightSelected()
         if showError:
-            board.checkIfCorrect()
+            board.checkInputIfCorrect()
         if board.keypressed:
             board.insertNumber(win)
 
     board.displayNumbers(win, board.initialBoard) #Displays the initial board
     board.displayNumbers(win, board.editedBoard) #Displays the edited board after input
     board.drawBoard()
+    board.inputBoxes(win)
+    text = pygame.font.SysFont("calibri", 30, 1)
+    value = text.render(time_format(play_time), 1, BLACK)
+    win.blit(value, (board.x + board.gridSize - 80, 166))
     pygame.display.update()
+
+def time_format(secs):
+    sec = secs % 60
+    minute = secs // 60
+    hour = minute // 60
+
+    formatted = str(minute) + " : " + str(sec)
+    return formatted
 
 
 board = Board(win, 50, 220, 630)
 board.editCheckerBoard()
 spacePressed = showError = isSolved = False
-cPressed = 0
+cPressed = mouseClicked = 0
+start = time.time()
 running = True
 while running:
+    play_time = round(time.time() - start)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
         if event.type == pygame.MOUSEBUTTONDOWN:
-            board.mousePos = pygame.mouse.get_pos()
+            if event.button == 1:
+                board.mousePos = pygame.mouse.get_pos()
+
+                #for play/pause button
+                if board.mousePos[0] > board.x + board.gridSize - 159 and board.mousePos[0] < board.x + board.gridSize - 124:
+                    if board.mousePos[1] > 160 and board.mousePos[1] < 200:
+                        if mouseClicked % 2 == 0:
+                            board.click = True
+                        else:
+                            board.click = False
+                        mouseClicked += 1
+
+            if event.button == 2:
+                pass
 
         if event.type == pygame.KEYDOWN:
             if not isSolved:
